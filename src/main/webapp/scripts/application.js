b09a2049f5ec2255cb55f8ac01eb9e1fd5426827
yannisf <yannis@ianos>
2014-09-29 22:16:39 +0300

    * Added bower * Added datepicker

--- src/main/webapp/scripts/application.js
+++ src/main/webapp/scripts/application.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('schoolApp', ['ngRoute'])
+angular.module('schoolApp', ['ngRoute', 'ui.bootstrap'])
 
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider
@@ -23,6 +23,14 @@ angular.module('schoolApp', ['ngRoute'])
             .otherwise({
                 redirectTo: '/child/list'
             });
+    }])
+
+    .run(['$rootScope', '$location', function($rootScope, $location) {
+        angular.extend($rootScope, {
+            cancel: function () {
+                $location.url('/child/list');
+            }
+        });
     }])
 
     .service('childService', ['$http', function ($http) {
@@ -70,13 +78,15 @@ angular.module('schoolApp', ['ngRoute'])
 
     .controller('createChildController', ['$scope', '$location', 'childService',
         function ($scope, $location, childService) {
-            $scope.data = {
-                child: null
-            };
-
-            $scope.cancel = function () {
-                $location.url('/child/list');
-            };
+            angular.extend($scope, {
+                data: {
+                    child: null
+                },
+                viewData: {
+                    allowDelete: false,
+                    submitLabel: 'Create'
+                }
+            });
 
             $scope.submit = function () {
                 childService.create($scope.data.child).then(
@@ -89,13 +99,16 @@ angular.module('schoolApp', ['ngRoute'])
 
     .controller('updateChildController', ['$scope', '$routeParams', '$location', 'childService',
         function ($scope, $routeParams, $location, childService) {
-            $scope.data = {
-                child: null
-            };
-
-            $scope.viewData = {
-                childId: $routeParams.id
-            }
+            angular.extend($scope, {
+                data: {
+                    child: null
+                },
+                viewData: {
+                    childId: $routeParams.id,
+                    allowDelete: true,
+                    submitLabel: 'Update'
+                }
+            });
 
             childService.fetch($scope.viewData.childId).then(
                 function (response) {
@@ -106,13 +119,13 @@ angular.module('schoolApp', ['ngRoute'])
             $scope.submit = function () {
                 childService.update($scope.viewData.childId, $scope.data.child).then(
                     function (response) {
+                        console.log('DOB: ', $scope.data.child.dateOfBirth);
                         $location.url('/child/list');
                     }
                 );
             };
 
             $scope.remove = function () {
-                console.log('Invoked remove');
                 childService.remove($scope.viewData.childId).then(
                     function (response) {
                         $location.url('/child/list');
@@ -120,8 +133,40 @@ angular.module('schoolApp', ['ngRoute'])
                 );
             };
 
-            $scope.cancel = function () {
-                $location.url('/child/list');
-            };
+
+            ////////////////////
+            $scope.today = function() {
+                $scope.dt = new Date();
+              };
+              $scope.today();
+
+              $scope.clear = function () {
+                $scope.dt = null;
+              };
+
+              // Disable weekend selection
+              $scope.disabled = function(date, mode) {
+                return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
+              };
+
+              $scope.toggleMin = function() {
+                $scope.minDate = $scope.minDate ? null : new Date();
+              };
+              $scope.toggleMin();
+
+              $scope.open = function($event) {
+                $event.preventDefault();
+                $event.stopPropagation();
+
+                $scope.opened = true;
+              };
+
+              $scope.dateOptions = {
+                formatYear: 'yy',
+                startingDay: 1
+              };
+              /////////////////
+
+
 
         }]);
