2ae5503fc4044e37328737f0c2fcaf3ebd846e79
yannis <yannis@sponde>
2014-10-18 09:50:37 +0300

    Bugfix in typeahead service

--- src/main/java/fraglab/school/typeahead/TypeAheadDaoImpl.java
+++ src/main/java/fraglab/school/typeahead/TypeAheadDaoImpl.java
@@ -17,7 +17,7 @@ public class TypeAheadDaoImpl extends GenericDaoImpl<Person, Long> implements Ty
     public List<String> findMatchingFirstNames(String startsWith) {
         Query query = entityManager.createQuery("select distinct p.firstName from Person p " +
                 "where lower(p.firstName) like :startsWith order by p.firstName");
-        query.setParameter("startsWith", startsWith + "%").setMaxResults(MAX_RESULTS);
+        query.setParameter("startsWith", startsWith.toLowerCase() + "%").setMaxResults(MAX_RESULTS);
         return query.getResultList();
     }
 
@@ -26,7 +26,7 @@ public class TypeAheadDaoImpl extends GenericDaoImpl<Person, Long> implements Ty
     public List<String> findMatchingLastNames(String startsWith) {
         Query query = entityManager.createQuery("select distinct p.lastName from Person p " +
                 "where lower(p.lastName) like :startsWith order by p.lastName");
-        query.setParameter("startsWith", startsWith + "%").setMaxResults(MAX_RESULTS);
+        query.setParameter("startsWith", startsWith.toLowerCase() + "%").setMaxResults(MAX_RESULTS);
         return query.getResultList();
     }
 
@@ -35,7 +35,7 @@ public class TypeAheadDaoImpl extends GenericDaoImpl<Person, Long> implements Ty
     public List<String> findMatchingProfessions(String startsWith) {
         Query query = entityManager.createQuery("select distinct g.profession from Guardian g " +
                 "where lower(g.profession) like :startsWith order by g.profession");
-        query.setParameter("startsWith", startsWith + "%").setMaxResults(MAX_RESULTS);
+        query.setParameter("startsWith", startsWith.toLowerCase() + "%").setMaxResults(MAX_RESULTS);
         return query.getResultList();
 
     }
@@ -45,7 +45,7 @@ public class TypeAheadDaoImpl extends GenericDaoImpl<Person, Long> implements Ty
     public List<String> findMatchingNationalities(String startsWith) {
         Query query = entityManager.createQuery("select distinct p.nationality from Person p " +
                 "where lower(p.nationality) like :startsWith order by p.nationality");
-        query.setParameter("startsWith", startsWith + "%").setMaxResults(MAX_RESULTS);
+        query.setParameter("startsWith", startsWith.toLowerCase() + "%").setMaxResults(MAX_RESULTS);
         return query.getResultList();
     }
 
@@ -54,7 +54,7 @@ public class TypeAheadDaoImpl extends GenericDaoImpl<Person, Long> implements Ty
     public List<String> findMatchingStreetNames(String startsWith) {
         Query query = entityManager.createQuery("select distinct a.streetName from Address a " +
                 "where lower(a.streetName) like :startsWith order by a.streetName");
-        query.setParameter("startsWith", startsWith + "%").setMaxResults(MAX_RESULTS);
+        query.setParameter("startsWith", startsWith.toLowerCase() + "%").setMaxResults(MAX_RESULTS);
         return query.getResultList();
     }
 
@@ -63,7 +63,7 @@ public class TypeAheadDaoImpl extends GenericDaoImpl<Person, Long> implements Ty
     public List<String> findMatchingNeighbourhoods(String startsWith) {
         Query query = entityManager.createQuery("select distinct a.neighbourhood from Address a " +
                 "where lower(a.neighbourhood) like :startsWith order by a.neighbourhood");
-        query.setParameter("startsWith", startsWith + "%").setMaxResults(MAX_RESULTS);
+        query.setParameter("startsWith", startsWith.toLowerCase() + "%").setMaxResults(MAX_RESULTS);
         return query.getResultList();
     }
 
@@ -72,7 +72,7 @@ public class TypeAheadDaoImpl extends GenericDaoImpl<Person, Long> implements Ty
     public List<String> findMatchingCities(String startsWith) {
         Query query = entityManager.createQuery("select distinct a.city from Address a " +
                 "where lower(a.city) like :startsWith order by a.city");
-        query.setParameter("startsWith", startsWith + "%").setMaxResults(MAX_RESULTS);
+        query.setParameter("startsWith", startsWith.toLowerCase() + "%").setMaxResults(MAX_RESULTS);
         return query.getResultList();
     }
 
