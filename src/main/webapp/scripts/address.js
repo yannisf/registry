2dd548c06988fb707e7e1432b5ec23a89a1470be
Ioannis Frangos (TRASYS GR) <ioannis.frangos@trasys.gr>
2015-01-29 14:41:03 +0200

    Typeaheads are required within address due to directive scoping issues

--- src/main/webapp/scripts/address.js
+++ src/main/webapp/scripts/address.js
@@ -68,6 +68,7 @@ angular.module('schoolApp')
                 scope: {
                     address: "=",
                     shareOption: "=",
+                    typeaheads: "="
                 },
                 templateUrl: "templates/input-address.html",
                 link: function (scope) {
