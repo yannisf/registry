bd55378fd41a79e09d7e9ee93657d5792010d9ec
yannisf <yannis@ianos>
2015-02-18 20:34:47 +0200

    Reverted back to HSQL and included a couple of migration

---
+++ src/main/resources/db/migration/V3__Renamed_column.sql
@@ -0,0 +1,2 @@
+alter table person drop column level;
+alter table person add PRESCHOOL_LEVEL varchar(255);
---
+++ src/main/resources/db/migration/V4__Renamed_column.sql
@@ -0,0 +1,2 @@
+alter table telephone drop column number;
+alter table telephone add num varchar(255);
