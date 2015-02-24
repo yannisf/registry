ae7f6561688f92338273bf262f9eb1b906b3c927
Ioannis Frangos (TRASYS GR) <ioannis.frangos@trasys.gr>
2015-02-24 11:58:41 +0200

    A directive to control subdirectives

--- src/main/webapp/management/school-control.js
+++ src/main/webapp/management/school-control.js
@@ -10,8 +10,13 @@ angular.module('management')
                 schools: "=",
                 viewData: "="
             },
+            require: ['^schoolControlCoordinator', 'schoolControl'],
             templateUrl: "management/school-control.tpl.html",
-            link: function(scope, element, attrs, controllers) {
+            link: function(scope, element, attrs, ctrls) {
+            	var schoolControlCoordinator, schoolControl;
+
+            	schoolControlCoordinator = ctrls[0];
+            	schoolControl = ctrls[1];
 
                 scope.remove = function() {
                     var index = scope.schools.indexOf(scope.school);
@@ -19,7 +24,49 @@ angular.module('management')
                     scope.viewData.activeSchool = null;
                     scope.viewData.activeGroup = null;
                 };
-                
-            }
+
+                scope.activate = function() {
+                	scope.editMode = true;
+                	schoolControlCoordinator.setActiveControl(schoolControl);
+                };
+
+                scope.deactivate = function() {
+                	scope.editMode = false;
+					schoolControlCoordinator.setActiveControl(null);
+                };
+
+                scope.$on('$destroy', function() {
+                	if( schoolControlCoordinator.getActiveControl === schoolControl ) {
+                		scope.deactivate();
+                	}
+                });
+            },
+            controller: ['$scope', function($scope) {
+				this.deactivate = function() {
+					$scope.editMode = false;
+				};
+            }]
         };
-    }]);
+    }])
+
+    .directive('schoolControlCoordinator', function() {
+    	return {
+    		restrict: 'A',
+    		scope: false,
+    		controller: function() {
+    			var activeControl;
+
+    			this.setActiveControl = function(ac) {
+    				if( activeControl != null ) {
+    					activeControl.deactivate();
+    				}
+    				activeControl = ac;
+    			};
+
+
+    			this.getActiveControl = function() {
+    				return activeControl;
+    			};
+    		}
+    	};
+    });
