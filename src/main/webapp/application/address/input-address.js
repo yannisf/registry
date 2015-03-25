cd300d348b4384aea8da150d0f366c41c58c20a3
yannisf <yannis@ianos>
2015-03-25 23:10:24 +0200

    typeaheads need not to be passed as parameters in input-address directive

--- src/main/webapp/application/address/input-address.js
+++ src/main/webapp/application/address/input-address.js
@@ -1,36 +1,34 @@
 'use strict';
 
-angular.module('schoolApp')
+angular.module('schoolApp').directive('inputAddress', ['$rootScope', 'uuid4', 'ChildService', 'AddressService',
+    function ($rootScope, uuid4, ChildService, AddressService) {
+        return {
+            replace: true,
+            restrict: 'E',
+            scope: {
+                address: "=",
+                allowCopy: "="
+            },
+            templateUrl: "application/address/input-address.tpl.html",
+            link: function (scope) {
+                scope.typeaheads = $rootScope.typeaheads;
 
-    .directive('inputAddress', ['ChildService', 'AddressService', 'uuid4',
-        function (ChildService, AddressService, uuid4) {
-            return {
-                replace: true,
-                restrict: 'E',
-                scope: {
-                    address: "=",
-                    allowCopy: "=",
-                    typeaheads: "="
-                },
-                templateUrl: "application/address/input-address.tpl.html",
-                link: function (scope) {
+                scope.viewData = {
+                    isAddressOpen: false
+                };
 
-                    scope.viewData = {
-                        isAddressOpen: false
-                    };
+                scope.clear = function() {
+                    scope.address = {id: scope.address.id};
+                };
 
-                    scope.clear = function() {
-                        scope.address = {id: scope.address.id};
-                    };
-
-                    scope.copyFromChild = function() {
-                        AddressService.getForPerson(ChildService.child.id).$promise.then(
-                            function(response) {
-                                response.id = scope.address.id;
-                                 scope.address = response;
-                            }
-                        );
-                    };
-                }
-            };
-        }]);
+                scope.copyFromChild = function() {
+                    AddressService.getForPerson(ChildService.child.id).$promise.then(
+                        function(response) {
+                            response.id = scope.address.id;
+                             scope.address = response;
+                        }
+                    );
+                };
+            }
+        };
+    }]);
