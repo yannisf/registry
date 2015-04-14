5c777c9ca29fe216e7784321bae365626e5d8847
yannisf <yannis@ianos>
2015-04-14 07:45:38 +0300

    Adding a login form

--- src/main/webapp/application/components/navbar.js
+++ src/main/webapp/application/components/navbar.js
@@ -1,21 +1,34 @@
 'use strict';
-angular.module('schoolApp').directive('navbar', ['$location', 'ActiveCache', function ($location, ActiveCache) {
-    return {
-        restrict: 'E',
-        replace: true,
-        scope: true,
-        templateUrl: "application/components/navbar.tpl.html",
-        controller: function($scope) {
-            $scope.active = ActiveCache;
-            
-            $scope.toOverview = function() {
-                ActiveCache.clearChild();
-                $location.url('/overview');
-            };
-            
-            $scope.toGroup = function() {
-                $location.url('/group/' + ActiveCache.group.id);
-            };
-        }
-    };
-}]);
+angular.module('schoolApp')
+    .directive('navbar', ['$location', 'ActiveCache', function ($location, ActiveCache) {
+        return {
+            restrict: 'E',
+            replace: true,
+            scope: true,
+            templateUrl: "application/components/navbar.tpl.html",
+            controller: function($scope) {
+                $scope.active = ActiveCache;
+                
+                $scope.toOverview = function() {
+                    ActiveCache.clearChild();
+                    $location.url('/overview');
+                };
+                
+                $scope.toGroup = function() {
+                    $location.url('/group/' + ActiveCache.group.id);
+                };
+            }
+        };
+    }])
+    
+    .directive('stopit', [function () {
+        return {
+            restrict: 'A',
+            scope: false,
+            link: function(scope, element) {
+                element.find('input').bind('click', function(event) {
+                    event.stopPropagation();
+                });
+            }
+        };
+    }]);
