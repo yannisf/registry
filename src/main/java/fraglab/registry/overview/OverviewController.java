92ea1d51ff137c2163b822568af0e09d4841d2e2
frangosi <ioannis.frangos@trasysgroup.com>
2015-09-15 10:56:19 +0300

    Removed unused imports

--- src/main/java/fraglab/registry/overview/OverviewController.java
+++ src/main/java/fraglab/registry/overview/OverviewController.java
@@ -7,7 +7,6 @@ import fraglab.web.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
