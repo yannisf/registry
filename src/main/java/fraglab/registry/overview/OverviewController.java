c4efd64acac5e727e30151e1b8cd832ffc87280c
yannisf <yannisf@ouranos>
2017-07-02 19:18:40 +0300

    Migrating to new spring controller mapping annotations

--- src/main/java/fraglab/registry/overview/OverviewController.java
+++ src/main/java/fraglab/registry/overview/OverviewController.java
@@ -29,79 +29,79 @@ public class OverviewController extends BaseRestController {
     @Autowired
     private ChildService childService;
 
-    @RequestMapping(value = "security", method = RequestMethod.GET)
+    @GetMapping(value = "security")
     public Authentication credentials() {
         return SecurityContextHolder.getContext().getAuthentication();
     }
 
-    @RequestMapping(value = "/school", method = RequestMethod.PUT)
+    @PutMapping(value = "/school")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void saveSchool(@RequestBody School school) {
         overviewService.saveSchool(school);
     }
 
-    @RequestMapping(value = "/school", method = RequestMethod.GET)
+    @GetMapping(value = "/school")
     public List<School> findSchools() {
         return overviewService.findSchools();
     }
 
-    @RequestMapping(value = "/school/{id}", method = RequestMethod.DELETE)
+    @DeleteMapping(value = "/school/{id}")
     @ResponseStatus(HttpStatus.ACCEPTED)
     public void deleteSchool(@PathVariable String id) {
         overviewService.deleteSchool(id);
     }
 
-    @RequestMapping(value = "/department", method = RequestMethod.GET)
+    @GetMapping(value = "/department")
     public List<Department> findDepartmentsForSchool(@RequestParam(value = "schoolId", required = true) String schoolId) {
         return overviewService.findDepartmentsForSchool(schoolId);
     }
 
-    @RequestMapping(value = "/department", method = RequestMethod.PUT)
+    @PutMapping(value = "/department")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void saveDepartmentForSchool(@RequestParam(value = "schoolId") String schoolId,
                                         @RequestBody Department department) {
         overviewService.saveDepartmentForSchool(schoolId, department);
     }
 
-    @RequestMapping(value = "/department/{id}", method = RequestMethod.DELETE)
+    @DeleteMapping(value = "/department/{id}")
     @ResponseStatus(HttpStatus.ACCEPTED)
     public void deleteDepartment(@PathVariable String id) {
         overviewService.deleteDepartment(id);
     }
 
-    @RequestMapping(value = "/group", method = RequestMethod.GET)
+    @GetMapping(value = "/group")
     public List<Group> findGroupsForDepartment(@RequestParam(value = "departmentId", required = true) String departmentId) {
         return overviewService.findGroupsForDepartment(departmentId);
     }
 
-    @RequestMapping(value = "/group", method = RequestMethod.PUT)
+    @PutMapping(value = "/group")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void saveGroup(@RequestBody Group group, @RequestParam(value = "departmentId") String departmentId) {
         overviewService.saveGroupForDepartment(group, departmentId);
     }
 
-    @RequestMapping(value = "/group/{id}", method = RequestMethod.DELETE)
+    @DeleteMapping(value = "/group/{id}")
     @ResponseStatus(HttpStatus.ACCEPTED)
     public void deleteGroup(@PathVariable String id) {
         overviewService.deleteGroup(id);
     }
 
-    @RequestMapping(value = "/group/{id}/child", method = RequestMethod.GET)
+    @GetMapping(value = "/group/{id}/child")
     public List<Child> findChildrenForGroup(@PathVariable String id) {
         return overviewService.findChildrenForGroup(id);
     }
 
-    @RequestMapping(value = "/group/{id}/statistics", method = RequestMethod.GET)
+    @GetMapping(value = "/group/{id}/statistics")
     public GroupStatistics findChildGroupStatistics(@PathVariable String id) {
         return overviewService.findChildGroupStatistics(id);
     }
 
-    @RequestMapping(value = "/group/{id}/info", method = RequestMethod.GET)
+    @GetMapping(value = "/group/{id}/info")
     public Map<String, Object> findGroupInfo(@PathVariable String id) throws NotFoundException {
         return overviewService.findGroupInfo(id);
     }
 
-    @RequestMapping(value = "/group/{groupId}/emails", method = RequestMethod.GET, produces = "text/plain; charset=UTF-8")
+    @GetMapping(value = "/group/{groupId}/emails", produces = "text/plain; charset=UTF-8")
     public void findGroupEmails(@PathVariable String groupId, HttpServletResponse response) throws IOException {
         byte[] result = childService.emailsForGroup(groupId).getBytes();
         response.addHeader("Content-Disposition", "attachment; filename=\"" + groupId + ".txt\"");
